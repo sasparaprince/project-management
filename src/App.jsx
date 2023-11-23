79e5e127dddd7558185682ae5984de98c5fad6c6
prince saspara <princesaspara46@gmail.com>
2023-11-23 15:58:48 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </>
+  );
+}
+export default App;
---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectsSidebar = () => {
+  return (
+    
+  )
+}
+
+export default ProjectsSidebar
